0251e0be8543921e3c67d558109e39486b3807fe
darshil040 <darshil040@gmail.com>
2025-07-26 10:27:12 +0530

    f1 score added

--- ml_example_original.py
+++ ml_example_original.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 # Load the Iris dataset
 iris = datasets.load_iris()
@@ -21,9 +22,12 @@ model.fit(X_train, y_train)
 # Make predictions
 y_pred = model.predict(X_test)
 
+f1 = f1_score(y_test, y_pred, average='macro')
+
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
 
 # Print the accuracy
-print ('Accuracy: ', accuracy*100)
+print('Accuracy: ', accuracy*100)
 
+print ('F1 score: ', f1)
